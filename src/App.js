e30b4939ecfa19d0fa2e206ec94a14d05cfc9b5a
raissaputra <raissaputra02@gmail.com>
2023-04-09 12:59:10 +0700

    created cards

--- src/App.js
+++ src/App.js
@@ -3,27 +3,56 @@ import "./App.css";
 import NavbarComponent from "./components/NavbarComponent";
 import ListCategories from "./components/ListCategories";
 import Hasil from "./components/Hasil";
+import React, { Component } from "react";
+import { API_URL } from "./utils/constants";
+import axios from "axios";
+import Menus from "./components/Menus";
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-                <hr/>
-              </h4>
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
-      </div>
-    </div>
-  );
+export default class App extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+     menus: [],
+  }
 }
 
-export default App;
+componentDidMount() {
+  axios.get(API_URL+"products")
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+}
+
+  render() {
+    const {menus} = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus && menus.map((menu => (
+                    <Menus
+                      key={menu.id}
+                      menu={menu}
+                    />
+                  )))}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
